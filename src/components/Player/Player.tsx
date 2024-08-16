046586917e65a007459a9a1999193b47cf5de7bd
Alexander Kacharava <alexander@cube.nl>
2024-08-16 10:52:22 +0200

    Add player name editing

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,18 +1,39 @@
+import React, {useState} from "react";
+
 export default function Player(
     props:{
         name:string,
         symbol:string
     }
 ) {
+    const [isEditing, setIsEditing] = useState(false);
+    const [playerName, setPlayerName] = useState(props.name);
+
+    const buttonText = isEditing ? "Save" : "Edit"
+
+    function handleEdit() {
+        setIsEditing((editing: boolean) => !editing)
+    }
+
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+        if (isEditing) {
+            setPlayerName(event.target.value)
+        }
+    }
+
     return (
         <li>
             <span className="player">
-                <span className="player-name">
-                    {props.name}
-                </span>
+                {
+                    isEditing ?
+                       <input type="text" value={playerName} onChange={handleChange} /> :
+                        <span className="player-name">
+                            {playerName}
+                        </span>
+                }
                 <span className="player-symbol">{props.symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleEdit}>{buttonText}</button>
         </li>
     )
 }
